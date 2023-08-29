f79a1836701881eacab3df1cdc539a20a4bc1ee7
djwt <danieljwtorres@yahoo.com>
2023-08-28 18:45:20 -0600

    runnit

---
+++ new.py
@@ -0,0 +1,1 @@
+maytricks is a versatile pentesting automated tool designed to comprehensively scan target systems for vulnerabilities and potential weaknesses. By seamlessly integrating the power of Nmap, Nikto, DirBuster, Gobuster, and Nuclei, maytricks delivers a robust scanning experience that covers network, web, and application security. 
