c9fc4864ef7cf2bb56aa51dfdc159e375a1f5cac
djwt <danieljwtorres@yahoo.com>
2023-09-17 23:25:48 -0600

    improved header

--- new.rb
+++ new.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
 require 'yaml'
 
-#Removes curses/Disable curses
+# This script provides a menu-driven interface for various tools.
+
+MENU_ITEMS = [
+  '1. Nmap',
+  '2. Nikto',
+  '3. DirBuster (via Python)',
+  '4. GoBuster (via Lua)',
+  '5. SQLMap',
+  '6. Exit'
+].freeze
 
 CONFIG = YAML.load_file('config.yml')
 
@@ -15,18 +24,9 @@ def init_screen
 end
 
 def display_menu
-  menu = [
-    '1. Nmap',
-    '2. Nikto',
-    '3. DirBuster (via Python)',
-    '4. GoBuster (via Lua)',
-    '5. SQLMap',
-    '6. Exit'
-  ]
-
   Curses.setpos(2, 2)
   Curses.addstr("Select a tool to run:")
-  menu.each_with_index do |item, index|
+  MENU_ITEMS.each_with_index do |item, index|
     Curses.setpos(4 + index, 4)
     Curses.addstr(item)
   end
@@ -50,6 +50,7 @@ def main
   loop do
     display_menu
     choice = get_user_input
+    break if choice == 6  # Exit choice
     handle_choice(choice, "target_ip")  # Update with your target IP logic
   end
 ensure
